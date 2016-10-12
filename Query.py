6da0d9db05fb61b2c370c2f2a273f43a8a6c5cb8
Tyler Worthington <worthingtont12@gmail.com>
2016-10-12 17:54:09 -0400

    Delete Query.py

--- Query.py
+++
@@ -1,9 +0,0 @@
-import re
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client['twitter1_db']
-collection = db['twitter_collection']
-tweets_iterator = collection.find()
-for tweet in tweets_iterator:
-        tmp = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet['text']).split())
-        print(tmp)
