c87987ab88ce363a9345469b9a7dfed1c2413d40
worthingtont12 <worthingtont12@gmail.com>
2016-10-12 22:25:19 -0400

    Updating Query to only print en for now

--- Data_Collection/Query.py
+++ Data_Collection/Query.py
@@ -5,5 +5,6 @@ db = client['twitter1_db']
 collection = db['twitter_collection']
 tweets_iterator = collection.find()
 for tweet in tweets_iterator:
+    if tweet['lang'] == 'en':
         tmp = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet['text']).split())
         print(tmp)
