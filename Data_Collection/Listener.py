9f9a4d79b0adbd7f85423f8ab077545aa1976eb0
worthingtont12 <worthingtont12@gmail.com>
2016-10-15 21:45:41 -0400

    working on getting psql set up

--- Data_Collection/Listener.py
+++ Data_Collection/Listener.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 import tweepy
 from tweepy import Stream
@@ -6,12 +7,9 @@ from tweepy.streaming import StreamListener
 import sys
 from pymongo import MongoClient
 import json
+import psycopg2
 
-# keys
-ckey = ''
-consumer_secret = ''
-access_token_key = ''
-access_token_secret = ''
+conn = psycopg2.connect("dbname='cultural_mapper' user='tylerworthington' host='localhost'")
 
 start_time = time.time()  # grabs the system time
 keyword_list = ['twitter']  # track list
@@ -24,55 +22,54 @@ class listener(StreamListener):
         self.limit = time_limit
         self.tweet_data = []
 
-    def on_data(self, data):
-
+    def on_data(self, status):
+        d = json.loads(status)
         while (time.time() - self.time) < self.limit:
-# check this out for more error handlers
-#thttp://stackoverflow.com/questions/23601634/how-to-restart-tweepy-script-in-case-of-error
             try:
-                client = MongoClient('localhost', 27017)
-                db = client['twitter1_db']
-                collection = db['twitter_collection']
-                tweet = json.loads(data)
-                # if tweet['coordinates'] is not None: If you only want geotagged tweets
-                collection.insert(tweet)
-                return True
-            # various exception handling blocks
-            except KeyboardInterrupt:
-                sys.exit()
-            except AttributeError as e:
-                print('AttributeError was returned, stupid bug')
-                print(e)
-                pass
-            except tweepy.TweepError as e:
-                print('Below is the printed exception')
-                print(e)
-                if '401' in e:
-                    # not sure if this will even work
-                    print('Below is the response that came in')
-                    print(e)
-                    time.sleep(60)
-                    pass
-                else:
-# raise an exception if another status code was returned,we don't like other kinds
-                    time.sleep(60)
-                    pass
+                    table = "tweet_LA"
+                    if table != "tweet_":
+                        cur = conn.cursor()
+                        command = ("INSERT INTO %s ( created_at, id, text, user_id, coordinates) VALUES ('%s','%s','%s','%s', ST_SetSRID(ST_MakePoint(%s, %s),4326));" % (table,datetime.datetime.strptime(d['created_at'],'%a %b %d %H:%M:%S +0000 %Y'),d['id'],d['text'].replace("'","''"),d['user']['id'],d['coordinates']['coordinates'][0],d['coordinates']['coordinates'][1])
+                        cur.execute(command)
+                        conn.commit()
+                        cur.close()
             except BaseException as e:
-                print('failed ondata,', str(e))
-                time.sleep(5)
-                pass
-        exit()
-
+                print("Error on_data: %s %s" % (str(e), status))
+                conn.rollback()
+            return True
     def on_error(self, status):
+            print(status)
+            return True
 
-        print(status)
 
-
-# Instance
-auth = OAuthHandler(ckey, consumer_secret)  # Consumer keys
-auth.set_access_token(access_token_key, access_token_secret)  # Secret Keys
-# initialize Stream object with a time out limit
-twitterStream = Stream(auth, listener(start_time, time_limit=120))
-# set bounding box filter
-twitterStream.filter(locations=[-118.723549, 33.694679, -117.929466, 34.33926])
-#Los Angeles
+try:
+    # start instance
+    auth = OAuthHandler(ckey, consumer_secret)  # Consumer keys
+    auth.set_access_token(access_token_key, access_token_secret)  # Secret Keys
+    # initialize Stream object with a time out limit
+    twitterStream = Stream(auth, listener(start_time, time_limit=120))
+    # set bounding box filter
+    twitterStream.filter(locations=[-118.723549, 33.694679, -117.929466, 34.33926])
+    # Los Angeles
+# various exception handling blocks
+except KeyboardInterrupt:
+    sys.exit()
+except AttributeError as e:
+    print('AttributeError was returned, stupid bug')
+    pass
+except tweepy.TweepError as e:
+    print('Below is the printed exception')
+    print(e)
+    if '401' in e:
+        # not sure if this will even work
+        print('Below is the response that came in')
+        print(e)
+        time.sleep(60)
+        pass
+    else:
+        # raise an exception if another status code was returned, we don't like other kinds
+        time.sleep(60)
+        pass
+except Exception as e:
+    time.sleep(60)
+    pass
