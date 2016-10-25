670d94745668af557866bd3a7f444ccbe2977a54
jrogol <rogol@virginia.edu>
2016-10-25 16:48:40 -0400

    Name Changed
    …to protect the innocent.

--- Data_Collection/LA/LA_Listener.py
+++ Data_Collection/LA/LA_Listener.py
@@ -9,13 +9,13 @@ from tweepy.streaming import StreamListener
 import psycopg2
 from keys import *
 
-conn = psycopg2.connect("dbname='culturalmapper_LA' user='culturalmapper' host='culturalmapper-la.cbjpxqmibsmf.us-east-1.rds.amazonaws.com' password='UVAdsi2017'")
+conn = psycopg2.connect("dbname='culturalmapper_LA' user='culturalmapper' host='culturalmapper-la.cbjpxqmibsmf.us-east-1.rds.amazonaws.com'")
 
 start_time = time.time()  # grabs the system time
 
 
 class listener(StreamListener):
-    def __init__(self, start_time, time_limit=300):
+    def __init__(self, start_time, time_limit=120):
 
         self.time = start_time
         self.limit = time_limit
@@ -25,7 +25,7 @@ class listener(StreamListener):
         while (time.time() - self.time) < self.limit:
             try:
                 cur = conn.cursor()
-                command = ("INSERT INTO la_city_primary(id, created_at, source, text, text_lang, user_id, user_location, user_handle, user_lang ) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (d['id'], (datetime.datetime.strptime(d['created_at'], '%a %b %d %H:%M:%S +0000 %Y')), d['source'], d['text'].replace("'", " "), d['lang'], d['user']['id'], d['user']['location'], d['user']['screen_name'], d['user']['lang']))
+                command = ("INSERT INTO city_primary(id, created_at, source, text, text_lang, user_id, user_location, user_handle, user_lang ) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (d['id'], (datetime.datetime.strptime(d['created_at'], '%a %b %d %H:%M:%S +0000 %Y')), d['source'], d['text'].replace("'", " "), d['lang'], d['user']['id'], d['user']['location'], d['user']['screen_name'], d['user']['lang']))
                 cur.execute(command)
                 conn.commit()
                 cur.close()
@@ -35,7 +35,7 @@ class listener(StreamListener):
             try:
                 if d['coordinates'] is not None:
                     cur = conn.cursor()
-                    command = ("INSERT INTO la_city_secondary( id, long, lat) VALUES ('%s', '%s', '%s');" % (d['id'], d['coordinates']['coordinates'][0], d['coordinates']['coordinates'][1]))
+                    command = ("INSERT INTO city_secondary( id, long, lat) VALUES ('%s', '%s', '%s');" % (d['id'], d['coordinates']['coordinates'][0], d['coordinates']['coordinates'][1]))
                     cur.execute(command)
                     conn.commit()
                     cur.close()
@@ -45,7 +45,7 @@ class listener(StreamListener):
             try:
                 if d['is_quote_status'] is True:
                     cur = conn.cursor()
-                    command = ("INSERT INTO la_quoted(id, q_id, q_created_at, q_text, q_text_lang, q_user_id, q_user_location, q_user_handle, q_user_lang ) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (d['id'], d['quoted_status']['id'], (datetime.datetime.strptime(d['quoted_status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')), d['quoted_status']['text'].replace("'", " "), d['quoted_status']['lang'], d['quoted_status']['user']['id'], d['quoted_status']['user']['location'], d['quoted_status']['user']['screen_name'], d['quoted_status']['user']['lang']))
+                    command = ("INSERT INTO quoted(id, q_id, q_created_at, q_text, q_text_lang, q_user_id, q_user_location, q_user_handle, q_user_lang ) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (d['id'], d['quoted_status']['id'], (datetime.datetime.strptime(d['quoted_status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')), d['quoted_status']['text'].replace("'", " "), d['quoted_status']['lang'], d['quoted_status']['user']['id'], d['quoted_status']['user']['location'], d['quoted_status']['user']['screen_name'], d['quoted_status']['user']['lang']))
                     cur.execute(command)
                     conn.commit()
                     cur.close()
@@ -55,7 +55,7 @@ class listener(StreamListener):
             try:
                 if d['user']['description'] is not None:
                     cur = conn.cursor()
-                    command = ("INSERT INTO la_user_desc(id, user_id, user_desc) VALUES ('%s','%s','%s');" % (d['id'], d['user']['id'], d['user']['description'].replace("'", " ")))
+                    command = ("INSERT INTO user_desc(id, user_id, user_desc) VALUES ('%s','%s','%s');" % (d['id'], d['user']['id'], d['user']['description'].replace("'", " ")))
                     cur.execute(command)
                     conn.commit()
                     cur.close()
@@ -76,7 +76,7 @@ try:
     auth = OAuthHandler(ckey, consumer_secret)  # Consumer keys
     auth.set_access_token(access_token_key, access_token_secret)  # Secret Keys
     # initialize Stream object with a time out limit
-    twitterStream = Stream(auth, listener(start_time, time_limit=300))
+    twitterStream = Stream(auth, listener(start_time, time_limit=120))
     # set bounding box filter
     twitterStream.filter(locations=[-118.723549, 33.694679, -117.929466, 34.33926])
     # Los Angeles
