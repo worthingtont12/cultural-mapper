94d4b7d160ba4e97b258a8a3dbd1e5562a872de2
worthingtont12 <worthingtont12@gmail.com>
2016-10-25 23:36:14 -0400

    parser language explainer

--- Parsing/Parsing_Template.py
+++ Parsing/Parsing_Template.py
@@ -1,12 +1,34 @@
 """Parse Tweets and export them into a working format for Topic Modeling."""
-
+import json
 import os
-import re
+#import re
 import pandas as pd
-os.chdir("CaseStudy2/Data/")
+os.chdir("/Users/tylerworthington/Git_Repos/Cultural_Mapper/Data")
+#importing json language file
+with open("Langauge.json") as json_file:
+    json_data = json.load(json_file)
+    print(json_data)
+#Import CSVs
+df = pd.read_csv("test.csv")
 
-#Import CSV
-df = pd.read_csv("tweets.csv")
+#transforming language variable
+print(df['lang'].value_counts())
+print(df['user_lang'].value_counts())
+
+languages = []
+for i in df['lang']:
+    for j in json_data:
+        if i == j['code']:
+            languages.append(j['name'])
+df['language'] = languages
+
+languages1 = []
+for i in df['user_lang']:
+    for j in json_data:
+        if i == j['code']:
+            languages1.append(j['name'])
+
+df['user_language'] = languages1
 
 #handling @
 
