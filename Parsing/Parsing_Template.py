c042f1c9c2e975022fd5cf15b1bd3fd835f3359b
worthingtont12 <worthingtont12@gmail.com>
2016-10-31 08:36:55 -0400

    updating parser

--- Parsing/Parsing_Template.py
+++ Parsing/Parsing_Template.py
@@ -1,59 +1,82 @@
 """Parse Tweets and export them into a working format for Topic Modeling."""
 import json
 import os
-#import re
+from string import punctuation
+import re
 import pandas as pd
-os.chdir("/Users/tylerworthington/Git_Repos/Cultural_Mapper/Data")
+os.chdir("/Users/tylerworthington/Git_Repos")
 #importing json language file
-with open("Langauge.json") as json_file:
+with open("Cultural_Mapper/Data/Langauge.json") as json_file:
     json_data = json.load(json_file)
-    print(json_data)
 #Import CSVs
-df = pd.read_csv("test.csv")
+primary = pd.read_csv("Data/la-primary_102616.csv", error_bad_lines=False)
+secondary = pd.read_csv("Data/la-secondary_102616.csv", error_bad_lines=False)
+quoted = pd.read_csv("Data/la-quoted102616.csv", error_bad_lines=False)
+user_desc = pd.read_csv("Data/la_user_desc_102616.csv", error_bad_lines=False)
 
 #transforming language variable
-print(df['lang'].value_counts())
-print(df['user_lang'].value_counts())
+print(primary['text_lang'].value_counts())
+print(primary['user_lang'].value_counts())
 
-languages = []
-for i in df['lang']:
-    for j in json_data:
-        if i == j['code']:
-            languages.append(j['name'])
-df['language'] = languages
+# languages = []
+# for i in primary['text_lang']:
+#     for j in json_data:
+#         if i == j['code']:
+#             languages.append(j['name'])
+# primary['language'] = languages
 
 languages1 = []
-for i in df['user_lang']:
+for i in primary['user_lang']:
     for j in json_data:
         if i == j['code']:
             languages1.append(j['name'])
 
-df['user_language'] = languages1
+primary['user_language'] = languages1
+print(primary['user_language'].value_counts())
 
 #handling @
 
 #handling links
 
-#stripping non text characters ie @, # ,https://, ect
-clean1 = []
-for i in df['text']:
-    tmp = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", i).split())
-    clean1.append(tmp)
-
-df['cleaned.text'] = clean1
-
-#collapsing tweets by user.id
-df['author.text'] = df[['user.id', 'cleaned.text']].groupby(['user.id'])['cleaned.text'].transform(lambda x: ','.join(x))
-
-#remove extra whitespace
-
-#remove punctuation
+#
+# #stripping non text characters ie @, # ,https://, ect
+# clean1 = []
+# for i in primary['text']:
+#     tmp = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", i).split())
+#     clean1.append(tmp)
+#
+# primary['cleaned.text'] = clean1
+#
+# #merge text from quoted with primary
+#
+# ##creating new df
+# #collapsing tweets by user.id
+# primary['author.text'] = primary[['user.id', 'cleaned.text']].groupby(['user.id'])['cleaned.text'].transform(lambda x: ','.join(x))
+#
+# #throwout duplicates
+#
+# #merge with user desc
+#
+# #Remove additional white spaces
+# primary['text'] = re.sub('[\s]+', ' ', primary['text'])
+#
+# #Convert www.* or https?://* to URL
+# primary['text'] = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL', primary['text'])
+#
+# #Replace #word with word
+# primary['text'] = re.sub(r'#([^\s]+)', r'\1', primary['text'])
+# #
+# # #remove punctuation
+# for p in list(punctuation):
+#     tweet_processed=tweet_processed.replace(p,'')
+# #or
+# df.replace({'\n': '<br>'}, regex=True)
 
 #ignore case
-
+str.lower()
 #remove stop words
 
 #stem all words
 
-#Export Tweets
-#df.to_csv('df.csv')
+# Export Tweets
+# primary.to_csv('primary.csv')
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ We are mapping out subpopulations using twitter data.
 We listened and stored tweets in Los Angeles, Chicago, and Istanbul for 3 months, 10/16-1/16.
 
 #Procedure
-After we collapsed all tweets by the same author we separated the tweets by language group. These language groups were used as corpora for topic modeling.
+After we collapsed all tweets by the same author we separated the tweets by language group. These language groups were used as corpora for topic modeling. 
 
 #Paper Title
 Visualizing Spatio-Temporal Trends in Urban Cultural Subpopulations through Social Media
