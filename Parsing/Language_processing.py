f753d286be79450426a0c662704f61400929cc7f
worthingtont12 <worthingtont12@gmail.com>
2016-11-29 10:12:58 -0500

    new features for processing file

--- Parsing/Language_processing.py
+++ Parsing/Language_processing.py
@@ -1,27 +1,15 @@
 """Processing Language for Topic Modeling."""
-import re
 import os
 import pandas as pd
-import nltk
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from io import StringIO
 
 os.chdir("/Users/tylerworthington/Git_Repos")
 
 df = pd.read_csv("author_tweets.csv", error_bad_lines=False)
-# recasting
-df['cleaned_user_desc'] = df['cleaned_user_desc'].apply(str)
-df['cleaned.q.author.text'] = df['cleaned.q.author.text'].apply(str)
-df['cleaned.author.text'] = df['cleaned.author.text'].apply(str)
 
-# merging all text into one column
-df['final_combined_text'] = df[['cleaned_user_desc', 'cleaned.q.author.text',
-                                'cleaned.author.text']].apply(lambda x: ' '.join(x), axis=1)
-
-# Function to deal with special characters, tokenizing,
+# Function to deal with tokenizing,stemming, and stop word filtering
 
 
 def process(text, lang):
@@ -43,22 +31,40 @@ def process(text, lang):
 
     return stop_words
 
+# recasting
+df['cleaned_user_desc'] = df['cleaned_user_desc'].apply(str)
+df['cleaned.q.author.text'] = df['cleaned.q.author.text'].apply(str)
+df['cleaned.author.text'] = df['cleaned.author.text'].apply(str)
+
+# merging all text into one column
+df['final_combined_text'] = df[['cleaned_user_desc', 'cleaned.q.author.text',
+                                'cleaned.author.text']].apply(lambda x: ' '.join(x), axis=1)
+
+# Stop Words
+# english
+english = stopwords.words('english')
+# Spanish
+spanish = stopwords.words('spanish')
+# Portuguese
+portuguese = stopwords.words('portuguese')
+# French
+french = stopwords.words('french')
+# German
+german = stopwords.words('german')
+# Russian
+russian = stopwords.words('russian')
+# Dutch
+dutch = stopwords.words('dutch')
+# Turkish
+turkish = stopwords.words('turkish')
+# Finnish
+finnish = stopwords.words('finnish')
+# Swedish
+swedish = stopwords.words('swedish')
 
 # applying function to dataframe
-english = stopwords.words('english')
 df_en = df[df.user_language == 'English']
 df_en['final_combined_text'] = df_en['final_combined_text'].apply(lambda row: process(row, english))
-
-# # Stop Words
-# English
-
-#
-# def stop(doc, lang):
-#     stop_words = " ".join([i for i in doc if i not in lang])
-#     return stop_words
-#
-#
-# df['final_combined_text1'] = df['final_combined_text'].apply(lambda doc: stop(doc, english))
 
 # exporting
 df_en.to_csv('cleaned_tweets.csv')
