857f8540faa18d1dea80631798ee6bf30032c5d3
worthingtont12 <worthingtont12@gmail.com>
2017-02-16 12:18:09 -0500

    single core

--- Topic_Modeling/Single_Core_Topic_Modeling.py
+++ Topic_Modeling/Single_Core_Topic_Modeling.py
@@ -1,24 +1,20 @@
-"""Topic Modeling on Authored Tweets Using Latent Dirichlet Allocation"""
-
-import smtplib
+"""Topic Modeling on Authored Tweets Using Latent Dirichlet Allocation."""
 import numpy as np
 import sklearn.feature_extraction.text as text
 from sklearn import decomposition
 from Parsing.Language_processing import df_en
-from Parsing.login_info import username, password2, recipient1
-
 
 # import data
 df_en['final_combined_text'] = df_en['final_combined_text'].apply(str)
 
+
 # create document term matrix
-vectorizer = text.CountVectorizer(max_df=0.95, min_df=200, strip_accents='unicode', lowercase=False)
+vectorizer = text.CountVectorizer(max_df=0.75, min_df=200, strip_accents='unicode', lowercase=False)
 dtm = vectorizer.fit_transform(df_en.final_combined_text).toarray()
 vocab = np.array(vectorizer.get_feature_names())
 
 # document term matrix size
 print(dtm.shape)
-print(((dtm.data.nbytes / 1024) / 1024))  # number of bytes dtm takes up
 
 # Parameters for topic model
 num_topics = 20
@@ -31,7 +27,7 @@ lda = decomposition.LatentDirichletAllocation(
 # train model
 doctopic = lda.fit_transform(dtm)
 
-# display results
+# Top words in topic models
 topic_words = []
 
 for topic in lda.components_:
@@ -40,12 +36,3 @@ for topic in lda.components_:
 
 for t in range(len(topic_words)):
     print("Topic {}: {}".format(t, ' '.join(topic_words[t][:15])))
-
-
-# email when done
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-
-server.login(username, password2)
-
-server.sendmail(username, recipient1, 'Topic Models Built')
