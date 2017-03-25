d510665aef00630bcf5128f7b5e253571fcb0d4d
James Rogol <jr2ef@virginia.edu>
2017-03-25 15:20:43 +0000

    Initializing AWS Instance

---
+++ AWSTimeSeries/LAX_meta.R
@@ -0,0 +1,10 @@
+# Database name for string
+ db <- "LA"
+
+# Bounding Box for Los Angeles
+bbox <- c(-118.723549, 33.694679, -117.929466, 34.33926)
+
+# UTM zone
+utm_zone <- 11
+
+epsg <- 2770
---
+++ AWSTimeSeries/keys.R
@@ -0,0 +1,3 @@
+usr = 'culturalmapper' 
+pwd = 'UVAdsi2017'
+hst = 'culturalmapper-la.cbjpxqmibsmf.us-east-1.rds.amazonaws.com'
