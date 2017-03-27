3a73df05c7e0447b11285f53b19983bcf02c0a47
worthingtont12 <worthingtont12@gmail.com>
2017-03-26 22:21:09 -0400

    first round time series analysis

--- Analysis/Time_Series.py
+++ Analysis/Time_Series.py
@@ -1,7 +1,6 @@
 """Time Series Analysis of Topic Activity"""
 import pandas as pd
 from pandas.tools.plotting import lag_plot
-from pandas import Series
 import statsmodels.api as sm
 from matplotlib import pyplot as plt
 # Time series
@@ -27,30 +26,34 @@ def test_stationarity(timeseries):
 
 # dropping first day of series
 fulldf = fulldf[fulldf.Date1 != '2016-10-28']
-
+# cast to string
+fulldf["top_topic"] = fulldf["top_topic"].apply(str)
 # list of 20 topics
 topics = (fulldf["top_topic"].unique())
 topics = list(topics)
 
+# loop through all topics to create graphs for each
 for i in topics:
     print("################Topic" + str(i) + "Results#################")
 
     # subset where topic equals i
-    tempdf = fulldf[fulldf.top_topic == i]
+    tempdf = fulldf[fulldf.top_topic == str(i)]
 
     # summing tweets for all days
-    Users = fulldf.groupby(['Date1'])['count'].aggregate(sum)
+    Users = tempdf.groupby(['Date1'])['count'].aggregate(sum)
 
     # descriptive statistics
     print(Users.describe())
 
     # density plots
     Users.plot(kind='kde')
-    plt.savefig("Graphs/Istanbul/Time_Series/density_Topic" + str(i) + ".png")
+    plt.savefig("Graphs/LA/Time_Series/density_Topic" + str(i) + ".png")
+    plt.close()
 
     # bar graphs
     Users.hist()
-    plt.savefig("Graphs/Istanbul/Time_Series/histogram_Topic" + str(i) + ".png")
+    plt.savefig("Graphs/LA/Time_Series/histogram_Topic" + str(i) + ".png")
+    plt.close()
 
     # rolling average plots
     fig = plt.figure(figsize=(10, 10))
@@ -60,7 +63,8 @@ for i in topics:
     fig = Users.rolling(window=6, center=False).mean().plot(label='Rolling 7 Day Mean')
     fig = Users.rolling(window=12, center=False).mean().plot(label='Rolling 14 Day Mean')
     fig = ts.legend(loc='best')
-    plt.savefig("Graphs/Istanbul/Time_Series/Rolling_Average_Plots_Topic" + str(i) + ".png")
+    plt.savefig("Graphs/LA/Time_Series/Rolling_Average_Plots_Topic" + str(i) + ".png")
+    plt.close()
 
     # ACF and PACF plots
     fig = plt.figure(figsize=(10, 10))
@@ -68,7 +72,8 @@ for i in topics:
     fig = sm.graphics.tsa.plot_acf(Users, lags=40, ax=ax1)
     ax2 = fig.add_subplot(212)
     fig = sm.graphics.tsa.plot_pacf(Users, lags=40, ax=ax2)
-    plt.savefig("Graphs/Istanbul/Time_Series/acf_pacf_plot_Topic" + str(i) + ".png")
+    plt.savefig("Graphs/LA/Time_Series/acf_pacf_plot_Topic" + str(i) + ".png")
+    plt.close()
 
     # testing stationarity
     print(test_stationarity(Users))
@@ -81,11 +86,13 @@ for i in topics:
     fig = sm.graphics.tsa.plot_acf(Usersdiff.dropna(inplace=False), lags=40, ax=ax1)
     ax2 = fig.add_subplot(212)
     fig = sm.graphics.tsa.plot_pacf(Usersdiff.dropna(inplace=False), lags=40, ax=ax2)
-    plt.savefig("Graphs/Istanbul/Time_Series/Differenced_acf_pacf_plot_Topic" + str(i) + ".png")
+    plt.savefig("Graphs/LA/Time_Series/Differenced_acf_pacf_plot_Topic" + str(i) + ".png")
+    plt.close()
 
     # lag plot
     lag_plot(Usersdiff)
-    plt.savefig("Graphs/Istanbul/Time_Series/lag_plot_Topic" + str(i) + ".png")
+    plt.savefig("Graphs/LA/Time_Series/lag_plot_Topic" + str(i) + ".png")
+    plt.close()
 
     # ARIMA model testing 1st lag
     mod1 = ARIMA(Usersdiff.dropna(inplace=False), order=(1, 1, 0))
@@ -97,4 +104,5 @@ for i in topics:
     plt.plot(Usersdiff.dropna(inplace=False))
     plt.plot(fit_AR.fittedvalues + Usersdiff.dropna(inplace=False).mean(), color='red')
     plt.title('Model Fit')
-    plt.savefig("Graphs/Istanbul/Time_Series/fitted_plot_Topic" + str(i) + ".png")
+    plt.savefig("Graphs/LA/Time_Series/fitted_plot_Topic" + str(i) + ".png")
+    plt.close()
