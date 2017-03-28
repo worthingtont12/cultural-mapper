83c4c3cfeb2aab62d04a69f60499205ae45f2700
James Rogol <jr2ef@virginia.edu>
2017-03-28 14:10:13 +0000

    Time series plots, and daily averages by topic

--- AWSTimeSeries/TS_Test.R
+++ AWSTimeSeries/TS_Test.R
@@ -8,21 +8,29 @@ ts.test3 <- ts(counts$`Topic 1 - Turkish`, freq  = 7)
 plot(ts.test)
 lines(ksmooth(1:length(ts.test), ts.test, 'normal', bandwidth = 4), col = 'red', lty = 2)
 
+#Decompose time series
 plot(stl(ts.test, "per"))
 
 Acf(ts.test2)
 Pacf(ts.test2)
-lag.plot(ts.test, lags = 7)
+lag.plot(ts.test3, lags = 7)
 
 fit <- auto.arima(ts.test2)
 fit
 
-Ccf(ts.test3, ts.test2)
+Ccf(ts.test3, ts.test2) 
+
 
 
 trend = ma(ts.test,order = 7, centre = T)
 plot(ts.test)
 lines(trend)
+
+
+autoplot.Arima(fit)
+autoplot.decomposed.ts(ts.test3)
+
+
 
 library(TSA)
 scatter.smooth(fit$residuals)
@@ -32,6 +40,12 @@ scatter.smooth(rstandard(fit))
 ndiffs(ts.test)
 # Number of SEASONAL differences required to make a series stationary
 nsdiffs(ts.test)
-auto.arima(ts.test2)
+auto.arima(ts.test3)
 
-Ccf(diff(ts.test), ts.test3)
+Ccf(ts.test, ts.test3)
+Pacf(ts.test3)
+
+fit <- auto.arima(ts.test3)
+fit
+plot(fit$fitted)
+Acf(fit$residuals)
